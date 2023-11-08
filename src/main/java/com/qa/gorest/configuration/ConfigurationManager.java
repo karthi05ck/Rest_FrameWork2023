e536d20471272cf0b2cb0abc1e354fada9838ba0
Karthick Selvaraj <karthise@VAKEN.CHN>
2023-11-08 17:37:34 +0530

    multi env code

--- src/main/java/com/qa/gorest/configuration/ConfigurationManager.java
+++ src/main/java/com/qa/gorest/configuration/ConfigurationManager.java
@@ -1,29 +1,71 @@
 package com.qa.gorest.configuration;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+
+import com.qa.gorest.frameworkexception.APIFrameworkException;
 
 public class ConfigurationManager {
 	
 	private Properties prop;
 	private FileInputStream ip;
 	
+	
+	
 	public Properties init_prop() {
 		prop = new Properties();
+		String envName = System.getProperty("env");
 		try {
+		if(envName==null) {
 			ip = new FileInputStream(".\\src\\test\\resources\\config\\config.properties");
-			prop.load(ip);
-		} catch (FileNotFoundException e) {
-			
-			e.printStackTrace();
-		} catch (IOException e) {
-			
+		}
+		else
+			switch (envName.toLowerCase()) {
+			case "qa":
+				ip = new FileInputStream(".\\src\\test\\resources\\config\\config.properties");
+				break;
+			case "dev":
+				ip = new FileInputStream(".\\src\\test\\resources\\config\\config_dev.properties");
+				break;
+			case "prod":
+				ip = new FileInputStream(".\\src\\test\\resources\\config\\config_prod.properties");
+				break;
+
+			default:
+				System.out.println("Please pass the correct environment...");
+				throw new APIFrameworkException("Please pass the valid env..The environment passed is :" +envName);
+				
+			}
+		
+		}
+		catch(FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		return prop;
+		try {
+			prop.load(ip);
+		}
+		catch(IOException e) {
+		
+		e.printStackTrace();
+		}
+	return prop;
 	}
 
+//		try {
+//			ip = new FileInputStream(".\\src\\test\\resources\\config\\config.properties");
+			//prop.load(ip);
+//		} catch (FileNotFoundException e) {
+//			
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			
+//			e.printStackTrace();
+//		}
+//		return prop;
+//	}
+
+	
+	
 }
